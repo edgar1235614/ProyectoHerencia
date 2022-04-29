0c77f389d371eb6f7540d4e9cea51dc548504162
edgar1235614 <101544939+edgar1235614@users.noreply.github.com>
2022-04-29 00:22:18 -0600

    Update Cliente.java
    Cambio Clase cliente

--- POO/src/ProyectoHerencia/Cliente.java
+++ POO/src/ProyectoHerencia/Cliente.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+
 public class Cliente extends Persona{//Encabezado de la clase Cliente
     //Atributos
     private int noCliente;
+    public static int totalClientes=0;
+    ArrayList <Producto> productos = new ArrayList<Producto>();
+    public void agregarProducto() {
+    	
+    }
+
     //Constructor
-    public Cliente(String nombre, String apellido, int edad, char sexo, int noCliente) {
+    public Cliente(String nombre, String apellido, int edad, char sexo) {
         super(nombre, apellido, edad, sexo);
+        totalClientes++;
+        noCliente=totalClientes;
     }
     //Metodos get y set
     public void setNoCliente(int num){
