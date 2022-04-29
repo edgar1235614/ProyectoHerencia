c28ac2793a610c91effac90c85bd178f2ae143bc
edgar1235614 <101544939+edgar1235614@users.noreply.github.com>
2022-04-29 00:23:06 -0600

    Update Producto.java
    Cambio clase Producto

--- POO/src/ProyectoHerencia/Producto.java
+++ POO/src/ProyectoHerencia/Producto.java
@@ -1,17 +1,19 @@
-/* Declaración de la clase Producto.java contiene atributos nombre, 
+/* DeclaraciÃ³n de la clase Producto.java contiene atributos nombre, 
  * precio y cantidad. Metodos: get/set para atributos y toString
-Se reutilizó la clase Producto T1U2   Fecha:  22/04/22  */
+Se reutilizÃ³ la clase Producto T1U2   Fecha:  22/04/22  */
 package ProyectoHerencia;
 public class Producto{
   //atributos
     private String nombre;
-    private int precio;
+    private double precio;
+    private double precioConIva;
     private int cantidad;
   //contructor con los 3 parametros para los atributos
-    public Producto(String nombre, int precio,int cantidad){
+    public Producto(String nombre, double precio,int cantidad){
     this.nombre =nombre;
     this.precio =precio;
     this.cantidad =cantidad;
+    precioConIva=precio+(precio*.16);
     }  
 
     @Override
@@ -28,20 +30,27 @@ public class Producto{
     }
     
     //Obtiene el precio del producto
-    public int getPrecio(){
+    public double getPrecio(){
       return this.precio;
     }
     //Cambia el precio del producto 
-    public void setPrecio(int precio){
+    public void setPrecio(double precio){
       this.precio=precio;
+      precioConIva=precio+(precio*.16);
     }
   //Obtiene la cantidad de productos
     public int getCantidad(){
       return this.cantidad;
     }
-    //Cambia el origen del producto 
+    //Cambia la cantidad de productos 
     public void setCantidad(int cantidad){
       this.cantidad=cantidad;
     }
+    public double getPrecioConIva(){
+        return this.precioConIva;
+      }
+    
+    
+    
    
 }//fin de la clase
